e21868f3fd1cf2c19bcad2d75777806e1e26fb6e
Dawid Margraf <David_neo1@wp.pl>
2019-02-22 15:06:08 +0100

    fixed buckles added spaces

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 'use strict';
 
 
@@ -15,139 +16,113 @@ var buttonF = document.getElementById('buttonF');
 var degreesC;
 var degreesF;
 
-buttonC.addEventListener('click', function()
-{	
-       degreesC = window.prompt('Prosze podać stopnie celcjusza');
-       var valueC = parseFloat(degreesC);
-       
-       var degreesF = degreesC * 1.8 + 32;  
-      
-       if(!isNaN(valueC))
-       {
-              output.innerHTML = 'Stopnie Celsjusza: ' + degreesC + '&#x2103 ' + '<br>' +
-              'Stopnie Fahrenheita: ' + degreesF + '&#x2109' + '<br>'; 
-       }
-       
-       else if (isNaN(valueC))
-       {
-              output.innerHTML = 'Nie wpisałeś cyfry';
-       }
+buttonC.addEventListener('click', function() {	
+    degreesC = window.prompt('Prosze podać stopnie celcjusza');
+    var valueC = parseFloat(degreesC);
+    
+    var degreesF = degreesC * 1.8 + 32;  
+    
+    if(!isNaN(valueC)) {
+            output.innerHTML = 'Stopnie Celsjusza: ' + degreesC + '&#x2103 ' + '<br>' + 'Stopnie Fahrenheita: ' + degreesF + '&#x2109' + '<br>'; 
+    }
+    
+    else if (isNaN(valueC)) {
+            output.innerHTML = 'Nie wpisałeś cyfry';
+    }
 
-       var outputinfo = document.getElementById('outputinfo');
-       
-       if(valueC == 0)
-       {
-              outputinfo.innerHTML = 'To jest temperatura zamarzania';
-       }
+    var outputinfo = document.getElementById('outputinfo');
+    
+    if(valueC == 0) {
+            outputinfo.innerHTML = 'To jest temperatura zamarzania';
+    }
 
-       else if(valueC == 100)
-       {
-              outputinfo.innerHTML = 'To jest temperatura wrzenia';
-       }
+    else if(valueC == 100) {
+            outputinfo.innerHTML = 'To jest temperatura wrzenia';
+    }
 
-       else if(valueC < 0)
-       {
-              outputinfo.innerHTML = 'Woda zmieniła się w lód';
-       }
+    else if(valueC < 0) {
+            outputinfo.innerHTML = 'Woda zmieniła się w lód';
+    }
 
-       else if(valueC > 100)
-       {
-              outputinfo.innerHTML = 'Woda zmieniła się w parę';
-       }
+    else if(valueC > 100) {
+            outputinfo.innerHTML = 'Woda zmieniła się w parę';
+    }
 
-       else if(valueC != null) 
-       {
-              outputinfo.innerHTML = '';
-       } 
+    else if(valueC != null) {
+            outputinfo.innerHTML = '';
+    } 
 
-       var outputinfo2 = document.getElementById('outputinfo2');
-       
-       if(valueC <= 0 )
-       {
-              outputinfo2.innerHTML = 'Należy ubrać cieplutkie ubranie';
-       }
-       
-       else if(valueC >= 20 && valueC < 40) 
-       {
-              outputinfo2.innerHTML = 'Można spokojnie ubrać krótkie spodenki';
-       }
+    var outputinfo2 = document.getElementById('outputinfo2');
+    
+    if(valueC <= 0 ) {
+            outputinfo2.innerHTML = 'Należy ubrać cieplutkie ubranie';
+    }
+    
+    else if(valueC >= 20 && valueC < 40) {
+            outputinfo2.innerHTML = 'Można spokojnie ubrać krótkie spodenki';
+    }
 
-       else if(valueC <= 10)
-       {
-              outputinfo2.innerHTML = 'Należy ubrać kurtkę i czapkę';
-       }
+    else if(valueC <= 10) {
+            outputinfo2.innerHTML = 'Należy ubrać kurtkę i czapkę';
+    }
 
-       else if(valueC != null) 
-       {
-              outputinfo2.innerHTML = '';
-       }
+    else if(valueC != null) {
+            outputinfo2.innerHTML = '';
+    }
 });
 
-buttonF.addEventListener('click', function()
-{	
-       degreesF = window.prompt('Prosze podać stopnie Fahrenheita');
-       var valueF = parseFloat(degreesF);
-       var degreesC = (degreesF -32) / 1.8;  
-       
-       if(!isNaN(valueF))
-       {
-              output.innerHTML = 'Stopnie Fahrenheita: ' + degreesF + '&#x2109 ' + '<br>' +
-              'Stopnie Celsjusza: ' + degreesC + '&#x2103' + '<br>'; 
-       }
-       
-       else if (isNaN(valueF))
-       {
-              output.innerHTML = 'Nie wpisałeś cyfry';
-       }    
+buttonF.addEventListener('click', function() {	
+    degreesF = window.prompt('Prosze podać stopnie Fahrenheita');
+    var valueF = parseFloat(degreesF);
+    var degreesC = (degreesF -32) / 1.8;  
+    
+    if(!isNaN(valueF)) {
+            output.innerHTML = 'Stopnie Fahrenheita: ' + degreesF + '&#x2109 ' + '<br>' + 'Stopnie Celsjusza: ' + degreesC + '&#x2103' + '<br>'; 
+    }
+    
+    else if (isNaN(valueF)) {
+            output.innerHTML = 'Nie wpisałeś cyfry';
+    }    
 
-       var outputinfo = document.getElementById('outputinfo');
+    var outputinfo = document.getElementById('outputinfo');
 
-       if(valueF == 32)
-       {
-              outputinfo.innerHTML = 'To jest temperatura zamarzania';
-       }
+    if(valueF == 32) {
+            outputinfo.innerHTML = 'To jest temperatura zamarzania';
+    }
 
-       else if(valueF == 212)
-       {
-              outputinfo.innerHTML = 'To jest temperatura wrzenia';
-       }
+    else if(valueF == 212) {
+            outputinfo.innerHTML = 'To jest temperatura wrzenia';
+    }
 
-       else if(valueF < 32)
-       {
-              outputinfo.innerHTML = 'Woda zamienia się w lód';
-       }
+    else if(valueF < 32) {
+            outputinfo.innerHTML = 'Woda zamienia się w lód';
+    }
 
-       else if(valueF > 212)
-       {
-              outputinfo.innerHTML = 'Woda zamienia się w parę';
-       }
+    else if(valueF > 212) {
+            outputinfo.innerHTML = 'Woda zamienia się w parę';
+    }
 
-       else if(valueF != null) 
-       {
-              outputinfo.innerHTML = '';
-       }
+    else if(valueF != null) {
+            outputinfo.innerHTML = '';
+    }
 
-       var outputinfo2 = document.getElementById('outputinfo2');
-      
-       if(valueF <= 32)
-       {
-              outputinfo2.innerHTML = 'Należy ubrać cieplutkie ubranie';
-       }
-       
-       else if(valueF >= 68 && degreesF < 104)
-       {
-              outputinfo2.innerHTML = 'Można spokojnie ubrać krótkie spodenki';
-       }
+    var outputinfo2 = document.getElementById('outputinfo2');
+    
+    if(valueF <= 32 ) {
+            outputinfo2.innerHTML = 'Należy ubrać cieplutkie ubranie';
+    }
+    
+    else if(valueF >= 68 && degreesF < 104) {
+            outputinfo2.innerHTML = 'Można spokojnie ubrać krótkie spodenki';
+    }
 
-       else if(valueF <= 50)
-       {
-              outputinfo2.innerHTML = 'Należy ubrać lekką kurtkę';
-       }
+    else if(valueF <= 50) {
+            outputinfo2.innerHTML = 'Należy ubrać lekką kurtkę';
+    }
 
-       else if(valueF != null) 
-       {
-              outputinfo2.innerHTML = '';
-       }
+    else if(valueF != null) {
+            outputinfo2.innerHTML = '';
+    }
 });
 
 
